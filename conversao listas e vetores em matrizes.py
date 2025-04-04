b2330386613ed429a5ba1310ba7406d54c4f7172
wanessa84 <wanessa.sant84@gmail.com>
2025-04-03 21:06:07 -0300

    Add files via upload

---
+++ conversao listas e vetores em matrizes.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+array = np.array ([0, 1, 2, 3, 4])
+lista = [0, 1, 2, 3, 4]
+matriz_1 = np.array([lista, lista, lista])
+print(matriz_1)
+print('----------------')
+matriz_2 = np.array([array, array, array])
+print(matriz_2)
+
