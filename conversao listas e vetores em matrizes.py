b5fdafdcf772f1b83527c4fe05ab11a454f7310b
wanessa84 <wanessa.sant84@gmail.com>
2025-04-03 21:20:44 -0300

    Update conversao listas e vetores em matrizes.py

--- conversao listas e vetores em matrizes.py
+++ conversao listas e vetores em matrizes.py
@@ -8,3 +8,16 @@ print('----------------')
 matriz_2 = np.array([array, array, array])
 print(matriz_2)
 
+#---------------------------------
+
+matriz_2 += 3       #Soma com matrizes
+print(matriz_2)
+
+matriz_2 -= 1      #subtração com matrizes
+print(matriz_2)
+
+matriz_2 *= 3
+print(matriz_2)
+
+matriz_2 = matriz_2/2
+print(matriz_2)
